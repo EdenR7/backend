2b6f3642e6fc6ca8b41e61c6f211683eda7e646e
EdenR7 <edenrot777@gmail.com>
2024-06-24 14:14:24 +0300

    Eden

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,6 +1,17 @@
 const fs = require("fs");
 const PRODUCTS = require("../data/db.json");
 
+function makeId(length = 5) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex];
+  }
+  return result;
+}
+
 function getProducts(req, res) {
   res.status(202).json(PRODUCTS);
 }
@@ -25,11 +36,12 @@ function deleteProduct(req, res) {
   res.status(200).json({ message: "Item deleted successful" });
 }
 function createProduct(req, res) {
-  // Add make Id
+  const newId = makeId();
   const { body: newProduct } = req;
+  newProduct._id = newId;
   const updatedProducts = [...PRODUCTS, newProduct];
   fs.writeFileSync("./data/db.json", JSON.stringify(updatedProducts));
-  res.status(201).json({ message: "Item Added successful" });
+  res.status(201).json({ message: "Item Added successful", item: newProduct });
 }
 function editProduct(req, res) {
   const { id } = req.params; //
--- index.js
+++ index.js
@@ -1,18 +1,54 @@
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// const PORT = process.env.PORT || 3000;
+
+// //MIDDLEWARE
+// app.use(express.json());
+// app.use(cors());
+
+// //ROUTES
+// const productRoutes = require("./routes/product.route");
+// app.use("/api/product", productRoutes); // When you reached this api go to this routes
+
+// // START SERVER
+// app.listen(PORT, () => {
+//   console.log(`Server Run at ${PORT}`);
+// });
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
 const cors = require("cors");
+
 const PORT = process.env.PORT || 3000;
+const connectDB = require("./config/db");
 
-//MIDDLEWARE
-app.use(express.json());
-app.use(cors());
+dotenv.config(); // Load config
 
-//ROUTES
-const productRoutes = require("./routes/product.route");
-app.use("/api/product", productRoutes); // When you reached this api go to this routes
+async function main() {
+  // Connect to database
+  await connectDB();
 
+  // MIDDLEWARES
+  // parse json body in request (for POST, PUT, PATCH requests)
+  app.use(express.json());
 
-// START SERVER
-app.listen(PORT, () => {
-  console.log(`Server Run at ${PORT}`);
-});
+  // allow CORS for local development (for production, you should configure it properly)
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+
+  // Routes
+  // const robotRoutes = require("./routes/robot.route");
+  // app.use("/api/robot", robotRoutes);
+  const productRoutes = require("./routes/product.route");
+  app.use("/api/product", productRoutes);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+main();
